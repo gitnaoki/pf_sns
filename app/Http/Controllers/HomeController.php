a53f3d6cad380f8bec2138f44786f991cf6c8072
gitnaoki <nlr999enjoy@gmail.com>
2021-09-02 19:54:07 +0900

    ログイン機能を追加

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(User $user)
+    {
+        // 現在ログインしているユーザー情報の取得
+        $user = Auth::user();
+        return view('home')->with(['user'=>$user]);
+    }
+}
